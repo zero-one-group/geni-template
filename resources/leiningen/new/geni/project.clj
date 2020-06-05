b6d1c16401da4096a31c96298aba72e56ae66250
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-05 15:40:05 +0700

    Version bump

--- resources/leiningen/new/geni/project.clj
+++ resources/leiningen/new/geni/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zero.one/geni "0.0.5"]]
+                 [zero.one/geni "0.0.6"]]
   :profiles {:provided
              {:dependencies [[ml.dmlc/xgboost4j-spark_2.12 "1.0.0"]
                              [ml.dmlc/xgboost4j_2.12 "1.0.0"]
