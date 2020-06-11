8c3d4037157e206a7654152a0b8e43930f19b506
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-11 21:40:46 +0700

    version bump to 0.07

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geni/lein-template "0.0.6"
+(defproject geni/lein-template "0.0.7"
   :description "Leiningen template for a Geni application."
   :url "https://github.com/zero-one-group/geni-template"
   :license {:name "Apache License"
--- resources/leiningen/new/geni/project.clj
+++ resources/leiningen/new/geni/project.clj
@@ -4,15 +4,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zero.one/geni "0.0.6"]]
+                 [zero.one/geni "0.0.7"]]
   :profiles {:provided
              {:dependencies [[ml.dmlc/xgboost4j-spark_2.12 "1.0.0"]
                              [ml.dmlc/xgboost4j_2.12 "1.0.0"]
-                             [org.apache.spark/spark-core_2.12 "2.4.5"]
-                             [org.apache.spark/spark-hive_2.12 "2.4.5"]
-                             [org.apache.spark/spark-mllib_2.12 "2.4.5"]
-                             [org.apache.spark/spark-sql_2.12 "2.4.5"]
-                             [org.apache.spark/spark-streaming_2.12 "2.4.5"]]}
+                             [org.apache.spark/spark-core_2.12 "2.4.6"]
+                             [org.apache.spark/spark-hive_2.12 "2.4.6"]
+                             [org.apache.spark/spark-mllib_2.12 "2.4.6"]
+                             [org.apache.spark/spark-sql_2.12 "2.4.6"]
+                             [org.apache.spark/spark-streaming_2.12 "2.4.6"]]}
              :uberjar {:aot :all}}
   :main ^:skip-aot {{namespace}}.core
   :target-path "target/%s")
