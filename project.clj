94c043a7d362e5fe451cf7ff5e7a17e3bdffa2d5
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-24 14:49:03 +0700

    version bump to 0.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geni/lein-template "0.0.10"
+(defproject geni/lein-template "0.0.11"
   :description "Leiningen template for a Geni application."
   :url "https://github.com/zero-one-group/geni-template"
   :license {:name "Apache License"
--- resources/leiningen/new/geni/project.clj
+++ resources/leiningen/new/geni/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [zero.one/geni "0.0.10"]]
+                 [zero.one/geni "0.0.11"]]
   :profiles {:provided
              {:dependencies [;; Spark
                              [org.apache.spark/spark-core_2.12 "3.0.0"]
