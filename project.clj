cca9b6fb23a419030c3dfc0e0de84d78c66f24ae
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-28 11:38:02 +0700

    Version bump to 0.0.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geni/lein-template "0.0.11"
+(defproject geni/lein-template "0.0.12"
   :description "Leiningen template for a Geni application."
   :url "https://github.com/zero-one-group/geni-template"
   :license {:name "Apache License"
