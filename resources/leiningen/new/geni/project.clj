c3986bb214f7b83decbe2f23ea2a503ebc8ab78b
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-07-01 22:09:55 +0700

    Version bump to 0.0.13 - project.clj

--- resources/leiningen/new/geni/project.clj
+++ resources/leiningen/new/geni/project.clj
@@ -4,13 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [zero.one/geni "0.0.13"]
                  ;; Spark
                  [org.apache.spark/spark-core_2.12 "3.0.0"]
                  [org.apache.spark/spark-hive_2.12 "3.0.0"]
                  [org.apache.spark/spark-mllib_2.12 "3.0.0"]
                  [org.apache.spark/spark-sql_2.12 "3.0.0"]
                  [org.apache.spark/spark-streaming_2.12 "3.0.0"]
-                 [zero.one/geni "0.0.12"]
                  ;; Optional: Spark XGBoost
                  [ml.dmlc/xgboost4j-spark_2.12 "1.0.0"]
                  [ml.dmlc/xgboost4j_2.12 "1.0.0"]
